bd759c193fb0d5374d7795e9fdba9255af51a211
DESKTOP-CF498JU\beggi <do.quanganh99zz@gmail.com>
2023-11-24 14:58:45 +0700

    update cau hinh

--- src/views/dashboard/DemoCauHinh.vue
+++ src/views/dashboard/DemoCauHinh.vue
@@ -4,121 +4,125 @@ import { Main } from '../styled';
 
 import DataTables from '@/components/table/DataTable.vue';
 import { BorderLessHeading } from '../styled';
-const dataTableColumn = [
+import ModalXoaNhomViecVue from './ModalXoaNhomViec.vue';
+import ModalThemMoiNhomViecVue from './ModalThemMoiNhomViec.vue';
+import ModalCapNhatNhomViecVue from './ModalCapNhatNhomViec.vue';
+
+const trangThai = [
   {
-    title: '#',
-    dataIndex: 'id',
-    key: 'id',
-    width: '5%',
-  },
-  {
-    title: 'Tên nhóm việc',
-    dataIndex: 'tenNhom',
-    key: 'tenNhom',
-    width: '25%',
-  },
-  {
-    title: 'Tình trạng việc',
-    dataIndex: 'tinhTrangViec',
-    key: 'tinhTrangViec',
-    width: '30%',
-  },
-  {
-    title: 'Thời gian',
-    dataIndex: 'thoiGian',
-    key: 'thoiGian',
-    width: '15%',
-  },
-  {
-    title: 'Số NS',
-    dataIndex: 'so',
-    key: 'so',
-    width: '15%',
+    title: 'Tên trạng thái',
+    dataIndex: 'tenTrangThai',
+    key: 'tenTrangThai',
+    width: '50%',
   },
   {
     title: 'Thao tác',
     dataIndex: 'action',
     key: 'action',
-    width: '10%',
+    width: '50%',
   },
 ];
-const DATA = [
+const vaiTro = [
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    title: 'Tên vai trò',
+    dataIndex: 'tenVaiTro',
+    key: 'tenVaiTro',
+    width: '50%',
   },
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    title: 'Thao tác',
+    dataIndex: 'action',
+    key: 'action',
+    width: '50%',
+  },
+];
+const loaiCongViec = [
+  {
+    title: 'Tên loại công việc',
+    dataIndex: 'tenLoaiCongViec',
+    key: 'tenLoaiCongViec',
+    width: '50%',
   },
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    title: 'Thao tác',
+    dataIndex: 'action',
+    key: 'action',
+    width: '50%',
+  },
+];
+const DATA_TRANGTHAI = [
+  {
+    tenTrangThai: 'Chuẩn bị',
   },
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    tenTrangThai: 'Chuẩn bị',
   },
+];
+const DATA_VAITRO = [
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    tenVaiTro: 'Chuẩn bị',
   },
+];
+const DATA_LOAICONGVIEC = [
   {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
-  },
-  {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
-  },
-  {
-    id: 1,
-    tenNhom: 'Heo lo',
-    thoiGian: 'thoi gian',
-    so: 123,
-    tinhTrangViec: 'Meo meo',
+    tenLoaiCongViec: 'Chuẩn bị',
   },
 ];
 
-const tableDataScource = computed(() =>
-  DATA.map((item: any) => {
+const tableDataScourceTrangThai = computed(() =>
+  DATA_TRANGTHAI.map((item: any) => {
     return {
-      id: item.id,
-      tenNhom: item.tenNhom,
-      thoiGian: item.thoiGian,
-      so: item.so,
-      tinhTrangViec: item.tinhTrangViec,
+      tenTrangThai: item.tenTrangThai,
       action: (
         <div class="table-actions">
           <router-link class="view" to={`#`}>
             <unicon name="eye"></unicon>
           </router-link>
           <router-link class="edit" to="#">
-            <unicon name="edit"></unicon>
+            <ModalCapNhatNhomViecVue />
           </router-link>
           <router-link class="delete" to="#">
-            <unicon name="trash"></unicon>
+            <ModalXoaNhomViecVue />
+          </router-link>
+        </div>
+      ),
+    };
+  }),
+);
+const tableDataScourceVaiTro = computed(() =>
+  DATA_VAITRO.map((item: any) => {
+    return {
+      tenVaiTro: item.tenVaiTro,
+      action: (
+        <div class="table-actions">
+          <router-link class="view" to={`#`}>
+            <unicon name="eye"></unicon>
+          </router-link>
+          <router-link class="edit" to="#">
+            <ModalCapNhatNhomViecVue />
+          </router-link>
+          <router-link class="delete" to="#">
+            <ModalXoaNhomViecVue />
+          </router-link>
+        </div>
+      ),
+    };
+  }),
+);
+const tableDataScourceLoaiCongViec = computed(() =>
+  DATA_LOAICONGVIEC.map((item: any) => {
+    return {
+      tenLoaiCongViec: item.tenLoaiCongViec,
+      action: (
+        <div class="table-actions">
+          <router-link class="view" to={`#`}>
+            <unicon name="eye"></unicon>
+          </router-link>
+          <router-link class="edit" to="#">
+            <ModalCapNhatNhomViecVue />
+          </router-link>
+          <router-link class="delete" to="#">
+            <ModalXoaNhomViecVue />
           </router-link>
         </div>
       ),
@@ -128,18 +132,57 @@ const tableDataScource = computed(() =>
 </script>
 
 <template>
-  <div class="nhom-viec">
+  <div class="cau-hinh">
     <Main>
-      <div class="fix-nhom-viec">
+      <div class="fix-cau-hinh">
         <a-row>
-          <a-col :xs="24" class="fix-nhom-viec">
+          <a-col :xs="24" class="fix-cau-hinh">
             <BorderLessHeading>
-              <sdCards title="Nhóm việc của tôi">
+              <sdCards title="Quản lý trạng thái">
+                <ModalThemMoiNhomViecVue />
                 <DataTables
                   :filterOption="true"
                   :filterOnchange="true"
-                  :tableData="tableDataScource"
-                  :columns="dataTableColumn"
+                  :tableData="tableDataScourceTrangThai"
+                  :columns="trangThai"
+                />
+              </sdCards>
+            </BorderLessHeading>
+          </a-col>
+        </a-row>
+      </div>
+    </Main>
+    <Main>
+      <div class="fix-cau-hinh">
+        <a-row>
+          <a-col :xs="24" class="fix-cau-hinh">
+            <BorderLessHeading>
+              <sdCards title="Quản lý vai trò">
+                <ModalThemMoiNhomViecVue />
+                <DataTables
+                  :filterOption="true"
+                  :filterOnchange="true"
+                  :tableData="tableDataScourceVaiTro"
+                  :columns="vaiTro"
+                />
+              </sdCards>
+            </BorderLessHeading>
+          </a-col>
+        </a-row>
+      </div>
+    </Main>
+    <Main>
+      <div class="fix-cau-hinh">
+        <a-row>
+          <a-col :xs="24" class="fix-cau-hinh">
+            <BorderLessHeading>
+              <sdCards title="Quản lý loại công việc">
+                <ModalThemMoiNhomViecVue />
+                <DataTables
+                  :filterOption="true"
+                  :filterOnchange="true"
+                  :tableData="tableDataScourceLoaiCongViec"
+                  :columns="loaiCongViec"
                 />
               </sdCards>
             </BorderLessHeading>
@@ -151,16 +194,16 @@ const tableDataScource = computed(() =>
 </template>
 
 <style scoped>
-.nhom-viec {
+.cau-hinh {
   font-family: Arial, Helvetica, sans-serif;
 }
-.nhom-viec .ant-card-head {
+.cau-hinh .ant-card-head {
   border-bottom: 0px;
 }
-.nhom-viec .lmeiNC {
+.cau-hinh .lmeiNC {
   padding-top: 20px;
 }
-.fix-nhom-viec {
+.fix-cau-hinh {
   height: 85vh;
   background-color: rgb(255, 255, 255);
 }
